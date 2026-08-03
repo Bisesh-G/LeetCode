98393916b59d89dca0cab0cce43ca16fa0c1a5ca
Bisesh-G <asmitbisesh0@gmail.com>
2026-08-03 13:27:22 +0530

    Time: 38 ms (5.31%) | Memory: 48.1 MB (91.53%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] nextSmaller(int[] arr, int n) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+
+        int[] ans = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = arr[i];
+
+            while (st.peek() != -1 && arr[st.peek()] >= curr) {
+                st.pop();
+            }
+
+            ans[i] = st.peek();
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    public int[] prevSmaller(int[] arr, int n) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int curr = arr[i];
+
+            while (st.peek() != -1 && arr[st.peek()] >= curr) {
+                st.pop();
+            }
+
+            ans[i] = st.peek();
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+
+        int n = heights.length;
+
+        int[] next = nextSmaller(heights, n);
+        int[] prev = prevSmaller(heights, n);
+
+        int area = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            int length = heights[i];
+
+            if (next[i] == -1) {
+                next[i] = n;
+            }
+
+            int breadth = next[i] - prev[i] - 1;
+
+            int newArea = length * breadth;
+
+            area = Math.max(area, newArea);
+        }
+
+        return area;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+
+        if (matrix.length == 0)
+            return 0;
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] arr = new int[cols];
+
+        int area = 0;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+
+                if (matrix[i][j] == '1')
+                    arr[j] += 1;
+                else
+                    arr[j] = 0;
+            }
+
+            area = Math.max(area, largestRectangleArea(arr));
+        }
+
+        return area;
+    }
+}
